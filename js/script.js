54e09fc9584fd84c4cacdf06e7980f5d79ca60c0
Bob Painter <kuroppi@protonmail.com>
2023-03-12 23:40:38 -0700

    Update code

--- js/script.js
+++ js/script.js
@@ -1,31 +1,21 @@
 // Global Variables
-let employeeId;
-let employeeName;
-let employeeExt;
-let employeeEmail;
-let employeeDepartment;
+let employeeId, employeeName, employeeExt, employeeEmail, employeeDepartment;
 
 // Helper Function To Return DOM Element
-function $(id) {
-  return document.getElementById(id);
-}
+const $ = (id) => document.getElementById(id);
 
 // Event Listener
-window.addEventListener("load", function () {
-  const empForm = $("empForm");
-  empForm.addEventListener("submit", function (e) {
-    e.preventDefault();
+window.addEventListener("load", () => {
+    const empForm = $("empForm");
+    empForm.addEventListener("submit", (e) => {
+        e.preventDefault();
 
-    employeeId = $("id").value;
-    employeeName = $("name").value;
-    employeeExt = $("ext").value
-    employeeEmail = $("email").value;
-    employeeDepartment = $("department").value;
+        employeeId = $("id").value;
+        employeeName = $("name").value;
+        employeeExt = $("ext").value
+        employeeEmail = $("email").value;
+        employeeDepartment = $("department").value;
 
-    console.log(`ID: ${employeeId}`);
-    console.log(`Name: ${employeeName}`);
-    console.log(`Extension: ${employeeExt}`);
-    console.log(`Email: ${employeeEmail}`);
-    console.log(`Department: ${employeeDepartment}`);
-  });
+        console.log(`ID: ${employeeId}\nName: ${employeeName}\nExtension: ${employeeExt}\nEmail: ${employeeEmail}\nDepartment: ${employeeDepartment}`);
+    });
 });
