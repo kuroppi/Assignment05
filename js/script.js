3d8f2e28f32cfed1bb90a7da84e5381a7a725fe6
Bob Painter <kuroppi@protonmail.com>
2023-03-12 23:08:03 -0700

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// Global Variables
+let employeeId;
+let employeeName;
+let employeeExt;
+let employeeEmail;
+let employeeDepartment;
+
+// Helper Function To Return DOM Element
+function $(id) {
+  return document.getElementById(id);
+}
+
+// Event Listener
+window.addEventListener("load", function () {
+  const empForm = $("empForm");
+  empForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    employeeId = $("id").value;
+    employeeName = $("name").value;
+    employeeEmail = $("email").value;
+    employeeDepartment = $("department").value;
+
+    console.log(`ID: ${employeeId}`);
+    console.log(`Name: ${employeeName}`);
+    console.log(`Email: ${employeeEmail}`);
+    console.log(`Department: ${employeeDepartment}`);
+  });
+});
